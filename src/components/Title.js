c1f8ddbf86a5b1aae113541a9b9f2709c5ac6fe0
Leonardo Muros do Nascimento <lmuros@gmail.com>
2021-04-03 23:05:22 -0300

    EMON finalizado

---
+++ src/components/Title.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+
+const Title = props => {
+  return (
+    <View style={[styles.container, props.style]}>
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.subTitle}>{props.subTitle}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 10},
+  title: {
+    fontFamily: 'Ubuntu-Bold', 
+    fontSize: 40, 
+    color: 'white', 
+    textAlign: 'center'},
+  subTitle: {
+    fontFamily: 'Ubuntu-Regular', 
+    fontSize: 14, 
+    color: 'white', 
+    textAlign: 'center'}
+})
+
+export default Title
